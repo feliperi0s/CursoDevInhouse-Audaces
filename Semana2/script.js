27d53fc6ec46735d816b8eb02d2198fe41c5f511
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-08 20:29:41 -0300

    aula js

---
+++ Semana2/script.js
@@ -0,0 +1,13 @@
+// CONSOLE.LOG('OLÁ MUNDO)
+
+
+var nome = prompt('digite seu nome')
+
+var sobrenome = prompt('digite seu sobrenome')
+
+var idade = prompt('digite sua idade')
+
+confirm("você é o " + nome + ' sobrenome ' + sobrenome + ' e idade ' +idade +' ? ')
+
+window.alert('Olá, eu sou '+ nome + ' '+sobrenome +' e tenho ' + idade+ ' anos de idade ')
+
