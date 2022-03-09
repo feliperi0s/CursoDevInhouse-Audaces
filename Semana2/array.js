ac8eddfaceb8f3a748b684171e83f6f9be990e60
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-08 21:22:05 -0300

    array

---
+++ Semana2/array.js
@@ -0,0 +1,20 @@
+var cores = ['vermelho', 'preto', 'branco', 'roxo', 'azul', 'amarelo'];
+
+console.log(cores[2])
+
+console.log(cores[0])
+
+console.log(cores[5])
+
+console.log(cores.length)
+
+var multicores = [
+    ['vermelho','preto','branco'],
+    ['roxo','azul']
+]
+
+console.log(multicores[0][0])
+console.log(multicores[1][0])
+console.log(multicores[0].length)
+console.log(multicores[1].length)
+console.log(multicores.length)
