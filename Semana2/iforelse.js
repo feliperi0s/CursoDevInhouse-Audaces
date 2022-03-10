567d7e4dd00b8310ff732857cedd51994c8e793d
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-09 21:57:12 -0300

    else if

---
+++ Semana2/iforelse.js
@@ -0,0 +1,57 @@
+// var resposta = confirm('deseja excluir o funcionario x ?')
+
+// if (resposta){
+
+// }
+// else {
+
+// }
+
+// var autenticado = false
+
+// if (autenticado === true) {
+//  console.log("bem vindo")
+// }
+
+// else {
+//     console.log("não autenticado")
+// }
+
+//  var idade = 17;
+
+//  var resultado;
+
+//  if (idade >= 18) {
+//     resultado = 'eh maior de idade';
+//  } else {
+//      resultado = 'eh menor de idade';
+//  }
+
+//  console.log(resultado);
+
+var idade = 29;
+var peso = 1.72
+var imc = peso / idade **2
+
+console.log(imc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
