fbea77ec245233f3353f7f3c68278f947c425ba9
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-10 20:07:39 -0300

    repetição

---
+++ Semana2/repeticao.js
@@ -0,0 +1,26 @@
+// // var positivos = 0;
+// // var negativos = 0;
+// // var zeros = 0;
+
+// // var numero = [5,7,-4,-5,0,9,-9,0];
+
+// // for(var i=0 ; i<numero.length ; i++ ) {
+// //     var nro = numero[i]
+// //     if (nro ===0) {
+// //         zeros++;
+// //     } else if (nro > 0 ){
+// //         positivos++;
+
+// //     } else {
+// //         negativos++;
+// //     }
+
+// // }
+
+// // console.log('positivos', positivos)
+// // console.log('negativos', negativos)
+// // console.log('zeros', zeros)
+
+
+
+
