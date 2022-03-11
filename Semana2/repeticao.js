f5e0180b462875292d0f0dc2e027bdc7c0d88110
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-10 21:23:20 -0300

    Update repeticao.js

--- Semana2/repeticao.js
+++ Semana2/repeticao.js
@@ -21,6 +21,36 @@
 // // console.log('negativos', negativos)
 // // console.log('zeros', zeros)
 
+// var valor = 1200;
+// var anos = 0;
 
+// while(valor < 5000 ) {
 
+//     var perc = valor * 0.10;
+//     valor += perc;
+//     anos++;
 
+// }
+
+// console.log(anos + ' anos')
+
+// for (var i = 0; i < 34; i++) {
+
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+
+// }
+
+var arr = [0, 1, 0, 1, 0, 1];
+
+for (var i=0; i< arr.length; i++){
+    var interruptor = (arr[i] === 0)? 'Desligado' : 'Ligado';
+    console.log(arr[i] + '-' + interruptor);
+}
