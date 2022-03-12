4f72180b8ba309484c450394ae14da0d88e2043d
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-12 12:04:03 -0300

    ex07

---
+++ Semana2/Exercicio/ex07/ex07.js
@@ -0,0 +1,11 @@
+var vitoria = 12
+var derrota = 2
+var empate = 6
+
+var partidas = vitoria + empate + derrota 
+
+var pontos = vitoria * 3 + empate
+
+console.log(pontos)
+console.log(partidas)
+console.log(`O time Tabajara F.C acumulou ${pontos} pontos após jogar ${partidas} partidas.`)
