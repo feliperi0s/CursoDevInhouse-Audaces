4f453b2e3d7effd41444c68e6f66a86d54f7e24b
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-03-12 14:44:59 -0300

    ex09

---
+++ Semana2/Exercicio/ex09/ex09.js
@@ -0,0 +1,11 @@
+var notas = [
+    [10, 8.5, 5.5, 9.5],
+    [8.5, 7, 7.5, 6],
+    [8, 9, 10, 7.5],
+    [9, 9.5, 10, 8]
+];
+var soma = notas[0][0] + notas[1][0] + notas[2][0] + notas[3][0]
+
+console.log(notas[2][0])
+console.log(notas[3][1])
+console.log(soma)
