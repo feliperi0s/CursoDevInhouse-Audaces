1776d86f279345c6197dc1a0a3a4b6d1b0e36563
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-04-05 21:47:12 -0300

    aula2

--- Semana6/aula2.js
+++ Semana6/aula2.js
@@ -1,25 +1,45 @@
-// Gera um número aleatório entre 0 e 9
-function obterNumeroAleatorio() {
-    const min = 0; 
-    const max = 9;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// // Gera um número aleatório entre 0 e 9
+// function obterNumeroAleatorio() {
+//     const min = 0; 
+//     const max = 9;
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-const numeroAletorio = new Promise ((resolve,reject)=>{
-    novonumero = obterNumeroAleatorio()
-    console.log(`Numero sorteado ${novonumero}`)
-    if (novonumero%2 == 0) {
-        resolve(novonumero * 2)
-    }else {
-        reject(novonumero)
+// const numeroAletorio = new Promise ((resolve,reject)=>{
+//     novonumero = obterNumeroAleatorio()
+//     console.log(`Numero sorteado ${novonumero}`)
+//     if (novonumero%2 == 0) {
+//         resolve(novonumero * 2)
+//     }else {
+//         reject(novonumero)
         
-    }
-})
+//     }
+// })
 
-numeroAletorio
-    .then( (valor) => {
-        console.log(`Numero dobrado ${valor}`);
+// numeroAletorio
+//     .then( (valor) => {
+//         console.log(`Numero dobrado ${valor}`);
+//     })
+//     .catch( (valor) => {
+//         console.log(valor);
+//     });
+
+
+//.................................................
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(res => {
+        console.log('resultado total', res);
+        return res;
     })
-    .catch( (valor) => {
-        console.log(valor);
-    });
+    .then(res => res.filter(user => user.id === 2 || user.id === 3) )
+    .then(res => 
+        res.map(user => {
+            return {...user, status: 'VERIFICADO'};
+        })
+    )
+    .then(res => console.log('resultado filtrado', res));
+    
+    
+    
