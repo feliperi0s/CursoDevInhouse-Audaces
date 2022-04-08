1ac8bd18c81a6db355163ab7752227dedc3dc168
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-04-07 21:23:51 -0300

    aula4

---
+++ Semana6/aula4.js
@@ -0,0 +1,11 @@
+async function obterDadosGit(){
+    const resp = await fetch('https://swapi.dev/api/people/1')
+    const dados = await resp.json();
+    const add = { ...dados, nave: "x-wing"};
+    const planeta = add.homeworld
+    const dadosplaneta0 = await fetch('https://swapi.dev/api/planets/1/')
+    const dadosPla = await dadosplaneta0.json();
+
+    console.log(`${dados.name} do planeta ${dadosPla.name} pilata uma ${add.nave}.`)
+}
+obterDadosGit()
