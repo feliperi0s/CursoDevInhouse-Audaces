0ad556955c0a8780431ab6eddd7d95c17e2b3771
feliperi0s <94755355+feliperi0s@users.noreply.github.com>
2022-04-07 21:44:34 -0300

    Update aula4.js

--- Semana6/aula4.js
+++ Semana6/aula4.js
@@ -3,7 +3,7 @@ async function obterDadosGit(){
     const dados = await resp.json();
     const add = { ...dados, nave: "x-wing"};
     const planeta = add.homeworld
-    const dadosplaneta0 = await fetch('https://swapi.dev/api/planets/1/')
+    const dadosplaneta0 = await fetch(planeta)
     const dadosPla = await dadosplaneta0.json();
 
     console.log(`${dados.name} do planeta ${dadosPla.name} pilata uma ${add.nave}.`)
